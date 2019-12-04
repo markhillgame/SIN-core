5e681a28d295f2d8d9ce1ba1e58789d88c94b783
markhillgame <markhillgame@gmail.com>
2019-12-04 23:35:28 +1030

    Update email.sh

--- contrib/SIN/infinitynode_expired_solution/email.sh
+++ contrib/SIN/infinitynode_expired_solution/email.sh
@@ -2,7 +2,8 @@
 # ----------------------------------------------------
 # Please install sendemail with the command line if you haven't done it yet
 # sudo apt-get install sendemail libio-socket-ssl-perl libnet-ssleay-perl
-# Once done, please edit email.sh and replace <FROM EMAIL>, <TO EMAIL>, smtp if you don't want to use google, <USERNAME>, <PASSWORD>
+# Once done, please edit email.sh and replace <FROM EMAIL>, <TO EMAIL>, <smtp> if you don't want to use google, <USERNAME>, <PASSWORD>
+# If you are using google, you will need to allow less secure app access or google will block sending email
 # Once configured, please change eENABLE=1
 
 eENABLED=0
